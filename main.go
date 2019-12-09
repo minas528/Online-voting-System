f3efb07a172d74f409ea77b5fd0e61431c75143e
minas528 <minalem.5d@gmail.com>
2019-12-09 09:36:02 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+
+var templ = template.Must(template.ParseGlob("ui/templates/*"))
+func main() {
+	fs := http.FileServer(http.Dir("ui/assets"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/assets/",http.StripPrefix("/assets",fs))
+	mux.HandleFunc("/",index)
+	http.ListenAndServe(":8989",mux)
+}
+
+func index(w http.ResponseWriter,r *http.Request)  {
+	templ.ExecuteTemplate(w, "index.html",nil)
+}
+
+
