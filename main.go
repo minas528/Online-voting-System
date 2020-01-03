7173b2a2e663e82128b12c39e29bb7edd62fcb79
bereket tusa <berekettussa@gmail.com>
2020-01-03 22:56:49 +0300

    new template added

--- main.go
+++ main.go
@@ -5,19 +5,17 @@ import (
 	"net/http"
 )
 
-
 var templ = template.Must(template.ParseGlob("ui/templates/*"))
+
 func main() {
 	fs := http.FileServer(http.Dir("ui/assets"))
 
 	mux := http.NewServeMux()
-	mux.Handle("/assets/",http.StripPrefix("/assets",fs))
-	mux.HandleFunc("/",index)
-	http.ListenAndServe(":8989",mux)
+	mux.Handle("/assets/", http.StripPrefix("/assets", fs))
+	mux.HandleFunc("/", index)
+	http.ListenAndServe(":8989", mux)
 }
 
-func index(w http.ResponseWriter,r *http.Request)  {
-	templ.ExecuteTemplate(w, "index.html",nil)
+func index(w http.ResponseWriter, r *http.Request) {
+	templ.ExecuteTemplate(w, "index.html", nil)
 }
-
-
