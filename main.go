a3c4aac99c041f5d351bebb20cf5ecc423954ac0
Saron <saronengida16@gmail.com>
2020-01-10 23:33:39 +0300

    Log in and sign up templates

--- main.go
+++ main.go
@@ -4,38 +4,39 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 
+	"html/template"
+	"net/http"
+
 	eventRepo "github.com/minas528/Online-voting-System/Event/repository"
 	eventServ "github.com/minas528/Online-voting-System/Event/service"
 	"github.com/minas528/Online-voting-System/delivery/http/handler"
 	postRepo "github.com/minas528/Online-voting-System/post/repository"
 	postServ "github.com/minas528/Online-voting-System/post/service"
-	"html/template"
-	"net/http"
 )
 
 var temp = template.Must(template.ParseGlob("ui/templates/*"))
 
-func login(w http.ResponseWriter, r *http.Request)  {
-	temp.ExecuteTemplate(w,"admin.voters",nil)
+func login(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "login", nil)
 }
-func signup(w http.ResponseWriter, r *http.Request)  {
-	temp.ExecuteTemplate(w,"signup",nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request)  {
-	temp.ExecuteTemplate(w,"",nil)
-}
-func newEvnet(w http.ResponseWriter,req *http.Request)  {
-	temp.ExecuteTemplate(w,"new.event",nil)
+func signup(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "signup", nil)
 }
 
-func RoutesForAdmin()  {
+func index(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "", nil)
+}
+func newEvnet(w http.ResponseWriter, req *http.Request) {
+	temp.ExecuteTemplate(w, "new.event", nil)
+}
+
+func RoutesForAdmin() {
 
 }
-func main()  {
+func main() {
 
-	dbconn,err := gorm.Open("postgres","postgres://postgres:minpass@localhost:9090/votes?sslmode=disable")
-	if err != nil{
+	dbconn, err := gorm.Open("postgres", "postgres://postgres:minpass@localhost:9090/votes?sslmode=disable")
+	if err != nil {
 		panic(err)
 	}
 
@@ -46,36 +47,25 @@ func main()  {
 	//	panic(errs)
 	//}
 
-
-
 	postRepo := postRepo.NewPostGormRepo(dbconn)
 	postserv := postServ.NewPostService(postRepo)
-	postHandler := handler.NewPostHandler(temp,postserv)
+	postHandler := handler.NewPostHandler(temp, postserv)
 
 	eventRep := eventRepo.NewEventRepository(dbconn)
 	eventserv := eventServ.NewEventService(eventRep)
-	eventHandle := handler.NewEventHandler(temp,eventserv)
-
-
-
-
+	eventHandle := handler.NewEventHandler(temp, eventserv)
 
 	fs := http.FileServer(http.Dir("ui/assets/"))
-	http.Handle("/assets/",http.StripPrefix("/assets",fs))
-	http.HandleFunc("/upost",postHandler.PostNew)
-	http.HandleFunc("/posts",postHandler.Posts)
-	http.HandleFunc("/",index)
+	http.Handle("/assets/", http.StripPrefix("/assets", fs))
+	http.HandleFunc("/upost", postHandler.PostNew)
+	http.HandleFunc("/posts", postHandler.Posts)
+	http.HandleFunc("/", index)
 	//http.HandleFunc("/newevent",newEvnet)
 
-	http.HandleFunc("/events",eventHandle.Events)
-	http.HandleFunc("/newevent",eventHandle.EventNew)
+	http.HandleFunc("/events", eventHandle.Events)
+	http.HandleFunc("/newevent", eventHandle.EventNew)
 
-	http.HandleFunc("/voters",login)
-	http.HandleFunc("/signup",signup)
-	http.ListenAndServe(":8181",nil)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/signup", signup)
+	http.ListenAndServe(":8181", nil)
 }
-
-
-
-
-
